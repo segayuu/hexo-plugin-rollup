0d3b422bc2421a69e9c36675eb68a897285320f2
segayuu <segayuu@gmail.com>
2017-08-15 13:17:03 +0900

    add renderer test

---
+++ test/renderer.js
@@ -0,0 +1,31 @@
+"use strict";
+const Hexo = require("hexo");
+const renderer = require("../lib/renderer");
+const { test } = require("ava");
+
+let hexo;
+let render;
+
+test.before(async () => {
+  hexo = new Hexo(process.cwd(), { silent: true });
+  render = renderer(hexo);
+  
+  hexo.config.rollup = {};
+
+  const initTask = hexo.init();
+  await initTask;
+
+  //call load() only initTask after.
+  //
+  const loadTask = hexo.load();
+  await loadTask;
+});
+
+test.beforeEach(t => {
+  hexo.config.rollup = {};
+})
+
+test("empty config", async t => {
+  const text = await render({path: "", text: ""});
+  t.deepEqual(text, "");
+});
